d4d2f69b627acf09ee37412061ff6527e1764d37
xuren <xuren@topjoy.com>
2023-05-19 18:07:21 +0800

    kcp

---
+++ common/src/main/java/com/xuren/game/common/net/kcp/GameKcpClient.java
@@ -0,0 +1,17 @@
+package com.xuren.game.common.net.kcp;
+
+import io.netty.buffer.ByteBuf;
+import org.beykery.jkcp.KcpClient;
+import org.beykery.jkcp.KcpOnUdp;
+
+public class GameKcpClient extends KcpClient {
+    @Override
+    public void handleReceive(ByteBuf byteBuf, KcpOnUdp kcpOnUdp) {
+
+    }
+
+    @Override
+    public void handleException(Throwable throwable, KcpOnUdp kcpOnUdp) {
+
+    }
+}
