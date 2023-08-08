f51267631626ce9f4c72a0e77216eec4eed4c947
xuren <xuren@topjoy.com>
2023-08-08 21:00:27 +0800

    向量

--- gameserver/src/main/java/com/xuren/game/logic/scene/utils/VectorUtils.java
+++ gameserver/src/main/java/com/xuren/game/logic/scene/utils/VectorUtils.java
@@ -14,11 +14,31 @@ public abstract class VectorUtils {
         return new Vector3f(x, y, z);
     }
 
+    public static Vector3f zero() {
+        return new Vector3f(0, 0, 0);
+    }
+
     public static double distance(Vector3f start, Vector3f end) {
         return Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2) + Math.pow(end.z - start.z, 2));
     }
 
     public static Vector3f sub(Vector3f v1, Vector3f v2) {
         return new Vector3f(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+    }
+
+    public static float getLengthSQ(Vector3f v) {
+        return v.x * v.x + v.y * v.y + v.z * v.z;
+    }
+
+    public static double getLength(Vector3f v) {
+        return Math.sqrt(getLengthSQ(v));
+    }
+
+    public static Vector3f normalize(Vector3f v) {
+        float len = (float) getLength(v);
+        v.x = v.x / len;
+        v.y = v.y / len;
+        v.z = v.z / len;
+        return v;
     }
 }
