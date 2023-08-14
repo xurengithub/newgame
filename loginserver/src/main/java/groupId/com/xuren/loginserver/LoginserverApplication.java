27c8a310ca77eccf6142c9a550ceda236e98f87e
xuren <xuren@topjoy.com>
2023-08-14 20:47:38 +0800

    loginserver

---
+++ loginserver/src/main/java/groupId/com/xuren/loginserver/LoginserverApplication.java
@@ -0,0 +1,13 @@
+package groupId.com.xuren.loginserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginserverApplication.class, args);
+	}
+
+}
