f33ab650d93aca458b9a11a86927e01b7afba3ab
xuren <xuren@topjoy.com>
2023-08-19 18:58:49 +0800

    zk注册

--- common/src/main/java/com/xuren/game/common/zk/ZKClient.java
+++ common/src/main/java/com/xuren/game/common/zk/ZKClient.java
@@ -1,8 +1,12 @@
 package com.xuren.game.common.zk;
 
+import com.xuren.game.common.utils.ZKUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author xuren
@@ -18,9 +22,23 @@ public class ZKClient {
             .sessionTimeoutMs(zkConfig.getSessionTimeoutMs())
             .namespace(zkConfig.getNamespace())
             .build();
+        registerNode();
     }
 
     public CuratorFramework getCuratorFramework() {
         return curatorFramework;
+    }
+
+    public static void registerNode() {
+        try {
+            String ip = InetAddress.getLocalHost().getHostAddress();
+
+            if (instance.curatorFramework.checkExists().forPath("/" + ZkConsts.GAME_NODE_PATH) == null) {
+                ZKUtils.create(instance.curatorFramework, "/" + ZkConsts.GAME_NODE_PATH, "".getBytes());
+            }
+            ZKUtils.createEphemeral(instance.curatorFramework, "/" + ZkConsts.GAME_NODE_PATH + "/" + ip, "".getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ common/src/main/java/com/xuren/game/common/zk/ZkConsts.java
@@ -0,0 +1,8 @@
+package com.xuren.game.common.zk;
+
+/**
+ * @author xuren
+ */
+public class ZkConsts {
+    public static final String GAME_NODE_PATH = "gameserver";
+}
